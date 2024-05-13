028deb9b9d85c346d4dbc9e68d0684d661ae6289
German <gercger5@gmail.com>
2024-05-13 09:28:59 -0300

    hola wrangler

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -53,7 +53,7 @@ export default function Home() {
             </span>
           </h2>
           <p className="m-0 max-w-[30ch] text-sm opacity-50">
-        HOLAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+        Hola entro wrangler
           </p>
         </a>
 
